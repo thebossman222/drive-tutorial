1ccd3c97161eb1d2e0d15617ca4840ca633d993a
Caleb Krainman <krainmcd@miamioh.edu>
2025-02-12 23:51:13 -0500

    Added database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
-import { type Config } from "drizzle-kit";
-
-import { env } from "~/env";
-
-export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["drive-tutorial_*"],
-} satisfies Config;
+// drizzle.config.ts
+import { defineConfig } from "drizzle-kit";
+import { env } from "process";
+export default defineConfig({
+    dialect: "singlestore",
+    schema: "./src/server/db/schema.ts",
+    dbCredentials: {
+        host: env.SINGLESTORE_HOST,
+        user: env.SINGLESTORE_USER,
+        password: env.SINGLESTORE_PASS,
+        database: env.SINGLESTORE_DB_NAME,
+        port: env.SINGLESTORE_PORT,
+	ssl: {},
+  maxIdle: 0, 
+    },
+});
