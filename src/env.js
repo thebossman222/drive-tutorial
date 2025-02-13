6d4dc2e40ae30a0cf16fd2158dec45a2eadcf9a9
Caleb Krainman <krainmcd@miamioh.edu>
2025-02-13 01:02:11 -0500

    Update SINGLESTORE_PORT type handling in env.js

--- src/env.js
+++ src/env.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
     SINGLESTORE_HOST: z.string(),
-    SINGLESTORE_PORT: z.string().regex(/^\d+$/).transform(Number),
+    SINGLESTORE_PORT: z.string(),
     SINGLESTORE_USER: z.string(),
     SINGLESTORE_PASS: z.string(),
     SINGLESTORE_DB_NAME: z.string(),
